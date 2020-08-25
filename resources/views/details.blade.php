2fb1f0f7cc62d1354fcafa79d0754b98177731ef
himanshu <himtweety@gmail.com>
2020-08-24 22:35:20 -0400

    qty added

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -33,6 +33,7 @@
                         <div class="card-body">
                             <h5 class="card-title">{{$product->name}}</h5>
                             <p class="card-text">{{$product->description}}</p>
+                            <p class="card-text">Qty Available : {{$product->units}}</p>
 
                             @auth
                             @if($product->units > 0)
